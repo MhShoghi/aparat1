99d9ae0a5d26e27808edef7aa24a36e62f7db8be
mhshoghi <hoseinshoghi@gmail.com>
2020-03-08 13:18:40 +0330

    created migrations, channel , followers, categories, videos , video-views , video-favourites , video-republishes , playlists , playlist-videos

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->string('email', 100)->unique();
+            $table->string('mobile', 13)->unique();
+            $table->string('avatar',100);
+            $table->string('password',100);
+            $table->string('website');
+            $table->string('verify_code',6)->nullable();
+            $table->timestamp('verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2020_03_08_094321_create_playlist_videos_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlaylistVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('playlist_videos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('playlist_id');
+            $table->unsignedBigInteger('video_id');
+
+
+
+
+            $table->foreign('playlist_id')
+                ->references('id')
+                ->on('playlists')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreign('video_id')
+                ->references('id')
+                ->on('videos')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('playlist_videos');
+    }
+}
