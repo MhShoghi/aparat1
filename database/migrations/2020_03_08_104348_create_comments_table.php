e577cce1d62e91ba9b842a0b8a5b88eb91672397
mhshoghi <hoseinshoghi@gmail.com>
2020-03-08 14:48:49 +0330

    created other migrations and relationships between video-report-categories and video-reports and created comments migration

---
+++ database/migrations/2020_03_08_104348_create_comments_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('user_id')->comment('UserID that send comment');
+            $table->unsignedBigInteger('video_id')->comment('Video that user send comment blow it');
+            $table->unsignedBigInteger('comment_id')->nullable()->comment('Sub comment');
+
+
+            $table->timestamp('accepted_at')->nullable();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreign('video_id')
+                ->references('id')
+                ->on('videos')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->timestamps();
+            $table->foreign('comment_id')
+                ->references('id')
+                ->on('comments')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
